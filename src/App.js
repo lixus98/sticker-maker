addcfc23f31d788a3facd271a51612746ebfd8fd
Daniel Riccio <luissonic96@gmail.com>
2021-01-07 21:56:34 -0500

    Add Home page and appbar component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Pages
+import Home from './Pages/Home';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Route
+        exact={true}
+        path='/home'
+        render={props => {
+          return(
+            <Home />
+          )
+        }}
+        />
+
+        
+      </Router>
+    )
+  }
 }
 
 export default App;
